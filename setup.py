dd9c973cd9df28579bafee98c6a91e9cef14f2cd
lamborghini1993 <1323242382@qq.com>
2019-11-06 17:15:58 +0800

    进度条提示

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 '''
 @Author: lamborghini1993
 @Date: 2019-09-29 17:41:58
-@UpdateDate: 2019-10-24 20:58:52
+@UpdateDate: 2019-11-06 17:11:17
 @Description: 发布包的配置
     https://packaging.python.org/tutorials/packaging-projects/#generating-distribution-archives
 '''
@@ -15,7 +15,7 @@ with open(os.path.join(os.path.dirname(__file__), "README.md")) as readme:
 
 setup(
     name='xhpubqt',
-    version="0.0.1",
+    version="0.0.2",
     author="lamborghini1993",
     author_email="1323242382@qq.com",
     description='a pub library for PyQt5 to implement some cool controls',
